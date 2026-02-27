37f14b76561ae8557f1e9d3d48fb1951020db1ac
Brielliana <miaxaulia@gmail.com>
2026-02-27 08:08:53 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,12 +6,18 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-# Konfigurasi
+# Ambil dari env atau pakai default
+DB_USER = os.getenv("DB_USER", "user")
+DB_PASS = os.getenv("DB_PASSWORD", "password")
+DB_NAME = os.getenv("DB_NAME", "taskdb")
+MINIO_USER = os.getenv("MINIO_USER", "user")
+MINIO_PASS = os.getenv("MINIO_PASSWORD", "password")
+
 BUCKET_NAME = "taskbucket"
-MINIO_CLIENT = Minio("minio:9000", access_key="admin_mimii", secret_key="password_minio", secure=False)
+MINIO_CLIENT = Minio("minio:9000", access_key=MINIO_USER, secret_key=MINIO_PASS, secure=False)
 
 def get_db_connection():
-    return psycopg2.connect(host="db", database="assignment_db", user="user_mimii", password="password_rahasia")
+    return psycopg2.connect(host="db", database=DB_NAME, user=DB_USER, password=DB_PASS)
 
 def init_db():
     print("Sedang menyiapkan database... ⏳")
@@ -41,16 +47,13 @@ def index():
     try:
         conn = get_db_connection()
         cur = conn.cursor()
-        # Coba ambil data
         try:
             cur.execute("SELECT assignment_name, file_path, submit_time, status FROM submissions ORDER BY submit_time DESC")
             data = cur.fetchall()
         except psycopg2.errors.UndefinedTable:
-            # KALAU TABEL ILANG, KITA BIKIN LAGI DI SINI! ✨
-            conn.rollback() # Reset transaksi yang gagal
-            init_db()       # Panggil fungsi buat tabel
-            data = []       # Kasih data kosong dulu biar gak error
-
+            conn.rollback()
+            init_db()
+            data = []
         cur.close()
         conn.close()
         return render_template('index.html', submissions=data)
@@ -65,6 +68,7 @@ def upload():
         if not MINIO_CLIENT.bucket_exists(BUCKET_NAME):
             MINIO_CLIENT.make_bucket(BUCKET_NAME)
         MINIO_CLIENT.put_object(BUCKET_NAME, file.filename, file, length=-1, part_size=10*1024*1024)
+        
         conn = get_db_connection()
         cur = conn.cursor()
         cur.execute("INSERT INTO submissions (assignment_name, file_path, submit_time, status) VALUES (%s, %s, %s, %s)",
@@ -75,5 +79,5 @@ def upload():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    init_db() # <--- INI KUNCI SEMBUHNYA!
+    init_db()
     app.run(host='0.0.0.0', port=5000)
